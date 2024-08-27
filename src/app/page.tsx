355795ca8382be562c61837e9c100f52dc17ad05
Iván <ivanlitt8@gmail.com>
2024-08-27 00:35:26 -0300

    feat: Responsive desing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,14 @@ import HealthLogh from "@/public/components/icons/HealthLogo";
 import StockRotationLogo from "@/public/components/icons/StockRotationLogo";
 import StockOutLogo from "@/public/components/icons/StockOutLogo";
 import heroBanner from "@/public/heroBanner.jpg";
+import { Space_Grotesk } from "@next/font/google";
+//import { useRouter, usePathname } from "next/navigation";
+//import * as gtag from '@/gtag.js';
+
+const spaceGrotesk = Space_Grotesk({
+  subsets: ["latin"], // Puedes agregar más subconjuntos si los necesitas
+  weight: ["400", "700"], // Puedes ajustar los pesos de la fuente
+});
 
 const cardsData = [
   {
@@ -48,8 +56,8 @@ export default function Home() {
         />
       </div>
       <main className="flex-1">
-        <div className="flex flex-col ml-20 items-center justify-center sm:flex-row sm:justify-between p-4 bg-white rounded-2xl sm:rounded-full shadow-md w-1/2 h-16 border border-lightgray relative bottom-5">
-          <span className="text-textprimary text-sm sm:text-base font-bold sm:pl-4 sm:pr-2 text-center sm:text-left mb-2 sm:mb-0 whitespace-nowrap">
+        <div className="flex flex-row mx-auto md:ml-32 items-center justify-between p-4 bg-white rounded-full shadow-md w-96 lg:w-2/3 xl:w-6/12 2xl:w-6/12 h-16 xl:h-24 2xl:h-32 border border-lightgray relative bottom-10">
+          <span className="text-textprimary text-base font-bold whitespace-nowrap lg:text-xl xl:text-2xl 2xl:text-4xl ml-10">
             ¡Activá Boty ahora mismo! →
           </span>
           <a
@@ -57,47 +65,51 @@ export default function Home() {
             href="https://app.boty.cloud/hello?utm_medium=facebook_remarketing_milbrands&utm_campaign=facturacion&boty_campaign=sales_billing.3"
             target="_blank"
             rel="noopener noreferrer"
-            className="flex items-center bg-btnprimary text-white font-medium py-2 px-4 rounded-full shadow-lg hover:bg-buttonhover mt-2 sm:mt-0 whitespace-nowrap text-base sm:text-lg"
+            className="flex items-center justify-center font-semibold bg-btnprimary text-white py-2 px-4 rounded-full shadow-lg hover:bg-buttonhover whitespace-nowrap text-lg xl:text-2xl 2xl:text-4xl w-40 xl:h-16 2xl:h-20"
           >
-            Nuevo
+            Activar
           </a>
         </div>
-        <div className="flex flex-col md:flex-row md:justify-around mx-10">
-          <div className="w-full md:w-2/5">
-            <h3 className="text-5xl font-bold text-textprimary">
+        <div className="flex flex-col md:flex-row md:justify-around lg:mx-14 xl:mx-40 2xl:mx-32">
+          <div className="w-full md:w-2/5 2xl:w-2/5">
+            <h3
+              className={`${spaceGrotesk.className} lg:text-3xl xl:text-5xl 2xl:text-7xl font-bold text-textprimary`}
+            >
               ¿Cansado de perder tiempo gestionando tu inventario?
             </h3>
-            <p className="w-full md:w-2/3 text-xl font-medium my-10">
+            <p className="lg:text-md xl:text-xl 2xl:text-4xl font-medium my-10">
               Boty te permite concentrarte en lo que realmente importa: hacer
               crecer tu negocio. Olvídate de las hojas de cálculo y las
               actualizaciones manuales. Con Boty, tenés un control total de tu
               stock en tiempo real.
             </p>
-            <button className="bg-btnprimary rounded-lg text-white text-lg font-medium w-48 p-3">
-              Accedé al módulo
+            <button className="bg-btnprimary lg:rounded-xl xl:rounded-3xl 2xl:rounded-3xl text-white xl:text-2xl 2xl:text-4xl font-medium w-auto lg:p-3 xl:p-4 2xl:p-6">
+              Activá Boty
             </button>
           </div>
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-8 md:mt-0">
+          <div className="flex flex-row flex-wrap gap-4 mt-8 justify-center">
             {cardsData.map((card, index) => (
               <div
                 key={index}
-                className="card p-6 w-full sm:w-64 h-72 bg-white rounded-lg shadow-lg transform transition-transform hover:scale-105"
+                className="card p-6 w-64 2xl:w-80 h-auto 2xl:h-96 bg-white rounded-lg shadow-lg transform transition-transform hover:scale-105"
               >
                 <div className="card-logo bg-iconbg flex items-center justify-center w-14 h-14 rounded-full mb-4">
                   {card.logo}
                 </div>
-                <h2 className="text-lg text-textprimary font-bold h-10 mb-6">
+                <h2
+                  className={`${spaceGrotesk.className} text-lg xl:text-2xl 2xl:text-3xl text-textprimary font-bold h-10 mb-6`}
+                >
                   {card.title}
                 </h2>
-                <p className="text-base font-medium text-textprimary">
+                <p className="text-base lg:text-sm xl:text-lg 2xl:text-2xl lg:mt-2 xl:mt-2 2xl:mt-10 font-semibold text-textprimary">
                   {card.text}
                 </p>
               </div>
             ))}
           </div>
         </div>
-        <div className="flex flex-col mt-10 items-center justify-center sm:flex-row sm:justify-between p-4 bg-white rounded-2xl sm:rounded-full shadow-md max-w-full mx-2 sm:mx-10 md:mx-32 border border-lightgray">
-          <span className="text-textprimary text-sm sm:text-base font-bold sm:pl-4 sm:pr-2 text-center sm:text-left mb-2 sm:mb-0 whitespace-nowrap">
+        <div className="flex flex-col mt-10 items-center justify-center sm:flex-row sm:justify-between lg:p-2 xl:p-4 2xl:p-6 bg-white rounded-2xl sm:rounded-full shadow-md max-w-full mx-2 sm:mx-10 md:mx-32 border border-lightgray">
+          <span className="text-textprimary lg:text-xl xl:text-2xl 2xl:text-4xl text-sm sm:text-base font-bold sm:pl-4 sm:pr-2 text-center sm:text-left mb-2 sm:mb-0 whitespace-nowrap">
             ¡Activa Boty ahora mismo!
           </span>
           <a
@@ -105,15 +117,15 @@ export default function Home() {
             href="https://app.boty.cloud/hello?utm_medium=facebook_remarketing_milbrands&utm_campaign=facturacion&boty_campaign=sales_billing.3"
             target="_blank"
             rel="noopener noreferrer"
-            className="flex items-center bg-btnprimary text-white font-medium py-2 px-4 rounded-full shadow-lg hover:bg-buttonhover mt-2 sm:mt-0 whitespace-nowrap text-base sm:text-lg"
+            className="flex items-center xl:text-2xl 2xl:text-4xl bg-btnprimary text-white font-medium lg:py-2 xl:py-4 2xl:py-4  px-6 rounded-full shadow-lg hover:bg-buttonhover mt-2 sm:mt-0 whitespace-nowrap text-base sm:text-lg"
           >
             Empecemos →
           </a>
         </div>
-        <div className="text-center pt-6 text-sm sm:font-semibold font-bold text-textprimary">
+        <div className="text-center pt-6 text-sm sm:font-semibold font-bold xl:text-xl 2xl:text-3xl text-textprimary">
           <p>Contratalo desde $30.000 por mes.</p>
         </div>
-        <footer className="w-full bg-btnprimary text-center mt-16 font-bold text-white py-4 text-xs sm:text-base">
+        <footer className="w-full bg-btnprimary text-center mt-16 font-bold text-white py-4 text-xs sm:text-base xl:text-xl 2xl:text-4xl">
           <span>¡Automatizá tu negocio con Boty y ahorrá tiempo y dinero!</span>
         </footer>
       </main>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,14 +27,14 @@ const config: Config = {
         lightgray: '#D9D9D9', 
         background: '#F8F7FD',
       },
-      // screens: {
+      screens: {
       //   'xs': '480px',
       //   'sm': '640px',   
-      //   'md': '768px',   
+      //   'md': '970px',   
       //   'lg': '1024px',  
       //   'xl': '1280px',  
       //   '2xl': '1536px',
-      // },
+      },
     },
   },
   plugins: [],
